8e275b50ea98dbb7a72c68429ea45c86d931da2f
alexandre236990 <109995421+alexandre236990@users.noreply.github.com>
2025-01-03 18:22:44 +0000

    Atualização batalha naval 1

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,35 @@
-#include <stdio.h>
+# include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+int main(){
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int navio[3][3] = {
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+        {0, 0, 3},
+        {0, 0, 0},
+        {3, 0, 0}
+    };
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    int x;
+    int y;
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    printf("\nEntre com a primeira coordenada sendo 0, 1 ou 2 para tenta acertar o navio: ");
+    scanf("%d", &x);
+
+    printf("\nEntre com a segunda coordenada sendo 0, 1 ou 2 para tenta acertar o navio: ");
+    scanf("%d", &y);
+    
+    if (navio[x][y] == 3) { 
+        
+        printf("\n\nParabens Vc acertou o navio\n\n");
+        
+         } else { 
+            
+            printf("\nVc errou o navio\n\n");
+        }
+    
+
+
 
     return 0;
 }
